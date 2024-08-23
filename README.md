cdf9ecea6e12e9082ff7eef59b794d9f315cd2e1
dlacorte10 <daniel.lacorte10@gmail.com>
2024-08-23 09:53:38 -0300

    Project created

---
+++ README.md
@@ -0,0 +1,6 @@
+This project was developed using some technologies listed below:
+
+- HTML and CSS;
+- Javascript;
+- Git and GitHub
+- Figma
---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')) {
+    img.setAttribute('src','./assets/avatar-light.png')
+    img.setAttribute('alt','Foto de Daniel Lacorte sorrindo, usando camisa preta')
+  } else {
+    img.setAttribute('src','./assets/avatar.png')
+    img.setAttribute(
+      'alt',"Foto de Daniel Lacorte sorrindo, usando camisa branca"
+    )
+  }
+}
